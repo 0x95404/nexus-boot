c579cd0220e3e4b99ee2653922a80389aa76227d
Sunny Runolfsdottir <KristopherlIl@hotmail.com>
2025-11-27 16:22:45 +0400

    Update main.rs

--- bootloader /src/main.rs
+++ bootloader /src/main.rs
@@ -1,25 +1,42 @@
 #![no_main]
 #![no_std]
 #![feature(abi_efiapi)]
+#![feature(alloc_error_handler)]
 
-mod gui;
-mod boot;
-mod pxe;
+extern crate alloc;
 
 use core::panic::PanicInfo;
 use limine_uefi::LimineBootServices;
 use uefi::prelude::*;
 use eframe::egui;
-use gui::{NexusApp, Theme};
+use crate::gui::{NexusApp, apply_theme};
+
+mod gui;
+mod boot;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! { loop {} }
+fn panic(info: &PanicInfo) -> ! {
+    log::error!("PANIC: {}", info);
+    loop {}
+}
+
+#[global_allocator]
+static ALLOC: uefi::alloc::Allocator = uefi::alloc::Allocator;
+
+#[alloc_error_handler]
+fn alloc_error(_layout: core::alloc::Layout) -> ! {
+    panic!("Memory allocation failed");
+}
 
 #[entry]
-fn efi_main(image: Handle, st: SystemTable<Boot>) -> Status {
-    let bt = st.boot_services();
-    eframe::start_efi(image, bt, |ctx| {
-        gui::apply_cyberpunk_theme(ctx);
+fn efi_main(image_handle: Handle, system_table: SystemTable<Boot>) -> Status {
+    uefi::helpers::init(&system_table).unwrap();
+    log::info!("NEXUS Boot v0.1 - The End of Ventoy");
+
+    let bt = system_table.boot_services();
+
+    eframe::start_efi(image_handle, bt, |ctx| {
+        apply_theme(ctx, "cyberpunk");
         Box::new(NexusApp::default())
     })
 }
